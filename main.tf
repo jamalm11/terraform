49c0e6d04f19b030fb3f7e48c2519bb20ad6d40e
vagrant <jamal.marouane@gmail.com>
2023-04-02 03:35:44 +0000

    add user-data: launch a web server at first start using busybox

--- main.tf
+++ main.tf
@@ -4,6 +4,13 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-007855ac798b5175e"
   instance_type = "t2.micro"
+user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
 
 tags = {
  name = "terraform-example"
